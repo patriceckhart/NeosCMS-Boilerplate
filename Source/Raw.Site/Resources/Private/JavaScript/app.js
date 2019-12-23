b492411533b67403ce786c16179f4989cfa68abf
Patric Eckhart <mail@patriceckhart.com>
2019-12-23 11:10:33 +0100

    Some modifications for Neos 5 and NeosRulez.Bootstrap 2.5. New NodeType SlickCarousel added.

--- Source/Raw.Site/Resources/Private/JavaScript/app.js
+++ Source/Raw.Site/Resources/Private/JavaScript/app.js
@@ -19,9 +19,34 @@ function resize() {
 
 $(document).ready(function(){
 
-    /*$('#toggle').click(function(){
+    $('#toggle').click(function(){
         $(this).toggleClass('open');
-    });*/
+    });
+
+    $('.slick-carousel-inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('.slick-carousel-inner, .carousel').show();
 
     resize();
 });
