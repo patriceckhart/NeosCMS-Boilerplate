bec09a7f197a166a3455d7dc7e126e976fe63131
Patric Eckhart <mail@patriceckhart.com>
2020-03-21 15:58:05 +0100

    bugfixes + changes in page template + "neosrulez/slickcarousel": "1.0" added

--- Source/Raw.Site/Resources/Private/JavaScript/app.js
+++ Source/Raw.Site/Resources/Private/JavaScript/app.js
@@ -23,31 +23,6 @@ $(document).ready(function(){
         $(this).toggleClass('open');
     });
 
-    $('.slick-carousel-inner').slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        fade: false,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: false,
-                }
-            }
-        ]
-    });
-
-    $('.slick-carousel-inner, .carousel').show();
-
     resize();
 });
 
